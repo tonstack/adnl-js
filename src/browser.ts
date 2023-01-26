b9fba48a376e7983cfda259bf00a5d409a87ca93
Tim <truecarry@gmail.com>
2023-01-26 21:07:01 +0300

    Add tcp client empty export for browser

--- src/browser.ts
+++ src/browser.ts
@@ -1,2 +1,8 @@
 export { ADNLClientWS } from './client-ws'
 export { ADNLClient } from './client'
+
+export class ADNLClientTCP {
+  constructor (url: string, peerPublicKey: Uint8Array | string) {
+    throw new Error('ADNLClientTCP is not available on this platform')
+  }
+}
